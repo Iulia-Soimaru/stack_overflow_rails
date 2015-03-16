e3ffd50f90d946c2f56891483e3d6f15439367ee
yaysunshine <merida1592@gmail.com>
2015-03-16 16:10:46 -0700

    restful routes for questions and answers

--- dbc_overflow/config/routes.rb
+++ dbc_overflow/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  resources :questions do
+    resources :answers
+  end
+  root 'questions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
