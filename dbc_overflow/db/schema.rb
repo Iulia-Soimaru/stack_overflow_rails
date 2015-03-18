8c68745a06cc3887099d1d1e55d3e2f59f5debe6
yaysunshine <merida1592@gmail.com>
2015-03-17 17:02:21 -0700

    add question_id to answers, complete all bullet points for release 1

--- dbc_overflow/db/schema.rb
+++ dbc_overflow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316233902) do
+ActiveRecord::Schema.define(version: 20150317221935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20150316233902) do
   create_table "answers", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
   end
 
   create_table "questions", force: :cascade do |t|
